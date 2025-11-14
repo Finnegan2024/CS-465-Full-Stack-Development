1026171a61063d3be89901b0c28808c0ed1edfda
Finnegan2024 <finnthomas2323@gmail.com>
2025-11-14 15:13:10 -0600

    Mod 3 debug

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel.js');
+var travelRouter = require('./app_server/routes/travel');
 var handlebars = require('hbs');
 
 var app = express();
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/travel');
 
-// Get travel page
+// Get travel view
 router.get('/', controller.travel);
 
 module.exports = router;
